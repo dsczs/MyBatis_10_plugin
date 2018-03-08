e09ef66789b9488fd8d24d1fef049907f76d1f71
dsczs <370637594@qq.com>
2018-03-08 14:59:35 +0800

    1

---
+++ src/com/atguigu/mybatis/dao/MySecondPlugin.java
@@ -0,0 +1,27 @@
+package com.atguigu.mybatis.dao;
+
+import java.util.Properties;
+
+import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.plugin.*;
+
+@Intercepts({ @Signature(type = StatementHandler.class, method = "parameterize", args = java.sql.Statement.class) })
+public class MySecondPlugin implements Interceptor {
+
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        System.out.println("MySecondPlugin...intercept:" + invocation.getMethod());
+        return invocation.proceed();
+    }
+
+    @Override
+    public Object plugin(Object target) {
+        System.out.println("MySecondPlugin...plugin:" + target);
+        return Plugin.wrap(target, this);
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+    }
+
+}
